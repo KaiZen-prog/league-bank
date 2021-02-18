3958122dbc5f24008f7ce8ace1973e6e4250171f
Николай Карнаев <karnaev.nick@gmail.com>
2021-02-18 22:24:41 +0300

    Настройка package.json и webpack

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+                    Hello World!
+        </p>
+      </header>
+    </div>
+  );
+};
+
+App.displayName = `App`;
+
+export default App;
